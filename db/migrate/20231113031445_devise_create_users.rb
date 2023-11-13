389da58ea9f5c23d7a54cb4bbd048755f1724e2d
ryuzaki3248 <gekidannax7@gmail.com>
2023-11-13 14:09:19 +0900

    カラム追加！！！！

--- db/migrate/20231113031445_devise_create_users.rb
+++ db/migrate/20231113031445_devise_create_users.rb
@@ -4,8 +4,24 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+
+
+
+
+
+
+      
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+
+      
+
+=begin       t.string :name,               null: false
+      t.string :profile,            null: false
+      t.string :occupation,         null: false
+      t.string :position,           null: false =end
+
+ 
 
       ## Recoverable
       t.string   :reset_password_token
