a79627c8176359a742025e57ba8b5d93f80fb959
ryuzaki3248 <gekidannax7@gmail.com>
2023-11-14 16:40:04 +0900

    ユーザー管理機能のパスを設定しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :user, presence: true
+         validates :name, presence: true
          validates :profile, presence: true
          validates :occupation, presence: true
          validates :position, presence: true
