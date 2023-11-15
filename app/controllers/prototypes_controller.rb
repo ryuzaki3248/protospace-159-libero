a6f87548e43a90c40bbfc89d49ac63b6bf0594dc
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2023-11-15 19:53:30 +0900

    コントローラーのprivateの位置を変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,14 +21,15 @@ class PrototypesController < ApplicationController
   end
 
 
+
+  
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept).merge(user_id: current_user.id)
-  end
-
-
-  def show
-    @prototype = Prototype.find(params[:id])
   end
 
 
