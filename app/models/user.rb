a64243b9419545da31d3223c061cd0eccd015bc0
ryuzaki3248 <gekidannax7@gmail.com>
2023-11-15 12:19:12 +0900

    3行を元に戻しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         validates :name, presence: true
+         validates :profile, presence: true
+         validates :occupation, presence: true
+
          belongs_to :prototype
 
 end
