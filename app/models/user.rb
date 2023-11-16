cf571300e521a1f9d2cb15595dc17099ad357a06
ryuzaki3248 <gekidannax7@gmail.com>
2023-11-16 12:42:46 +0900

    Commentモデルに、userおよびprototypeに対するアソシエーションを設定した2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         belongs_to :comment
+
   validates :name, presence: true
   validates :profile, presence: true
   validates :occupation, presence: true
