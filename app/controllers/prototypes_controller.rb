d5f4b9054ecc0aef5063655bc45cfd0a625f027e
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2023-11-16 20:27:10 +0900

    編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -30,8 +30,8 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find (params[:id])
-    if prototype.update(prototype_params)
+    @prototype = Prototype.find (params[:id])
+    if @prototype.update(prototype_params)
       redirect_to root_path
     else
       render :edit, status: :unprocessable_entity
