2db66fc49a63f8ba2490dc8706ba7aa43683384f
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2023-11-16 12:56:24 +0900

    編集ボタンの成功、失敗の処理を記述

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -30,9 +30,11 @@ class PrototypesController < ApplicationController
 
   def update
     prototype = Prototype.find (params[:id])
-    prototype.update(prototype_params)
-    redirect_to root_path
-    
+    if prototype.update(prototype_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def show
