55f54e55c1f3f0455791dcd7536f9ee383c36279
ryuzaki3248 <gekidannax7@gmail.com>
2023-11-16 12:52:06 +0900

    UserモデルおよびPrototypeモデルにも、commentに対するアソシエーションを設定した（必要に応じてdependent: :destroyオプションを使用した）

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   has_many :users
   has_many :prototypes
+
+	belongs_to :user
+	belongs_to :prototype
+
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,11 +1,14 @@
 class Prototype < ApplicationRecord
 	belongs_to :user
 	belongs_to :comment
+	
 	has_one_attached :image
 
 	validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true
 	validates :image, presence: true
+
+	has_many :comments, dependent: :destroy
 	
 end
