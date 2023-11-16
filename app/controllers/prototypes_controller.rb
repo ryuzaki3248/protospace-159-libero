4ac2cf9415289260b0ae3cb63c89adc34b67a9a6
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2023-11-16 11:47:07 +0900

    updateコントローラーを記述

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -23,6 +23,9 @@ class PrototypesController < ApplicationController
   end
 
   def update
+    prototype = Prototype.find (params[:id])
+    prototype.update(prototype_params)
+    redirect_to root_path
     
   end
 
