f1576785614c0fc5f908eee042d085ca63a46491
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2023-11-16 21:44:01 +0900

    違うユーザーからは編集できない

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,6 +26,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find (params[:id])
+    if current_user.id != @prototype.user_id
+      redirect_to root_path
+    end
 
   end
 
