60eba53ff8f79ccfe13a0516da58f82c2e276fa3
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2023-11-17 12:59:12 +0900

    コントローラーにcreateアクションを記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,13 @@
 class CommentsController < ApplicationController
-	def create
   
+	def create
+    @comments = Comment.create(comment_params)
   end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).marge(user_id: current_user.id,prototype_id: params[:tweet_id])
+  end
+    
+
 end
